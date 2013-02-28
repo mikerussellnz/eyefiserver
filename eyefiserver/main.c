7746e7dff7d5cfddd50442ea1898f31c44cb5dbf
Mike Russell <mikerussellnz@gmail.com>
2013-02-28 21:17:11 +1300

    Initial Commit

---
+++ eyefiserver/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  eyefiserver
+//
+//  Created by Michael Russell on 28/02/13.
+//  Copyright (c) 2013 Michael Russell. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
